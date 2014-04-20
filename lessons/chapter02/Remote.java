2cb0749a5cd7da55e4757a4028343e043176ffbb
shgtkshruch <shgtk.shruch@gmail.com>
2014-04-20 15:39:08 +0900

    Add chapter02

---
+++ lessons/chapter02/Remote.java
@@ -0,0 +1,27 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Remote {
+
+  private DogDoor door;
+
+  public Remote(DogDoor door) {
+    this.door = door;
+  }
+
+  public void pressButton() {
+    System.out.println("Push remote controller buttons.");
+    if(door.isOpen()) {
+      door.close();
+    } else {
+      door.open();
+      final Timer timer = new Timer();
+      timer.schedule(new TimerTask() {
+        public void run() {
+          door.close();
+          timer.cancel();
+        }
+      }, 5000);
+    }
+  }
+}
